d413e7fcdb341e79fd54fa7ee655fdf0a23d49e7
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2016-07-25 07:37:12 +0800

    Update billplz.php

--- includes/languages/english/modules/payment/billplz.php
+++ includes/languages/english/modules/payment/billplz.php
@@ -10,11 +10,11 @@
 ob_start();
 
     define('MODULE_PAYMENT_BILLPLZ_TEXT_CATALOG_TITLE', 'Internet Banking (FPX) Payment');
-    define('MODULE_PAYMENT_BILLPLZ_TEXT_ADMIN_TITLE', 'Billplz Online Payment Gateway(Visa, MasterCard, Maybank2u, MEPS, FPX, etc)');
-    define('MODULE_PAYMENT_BILLPLZ_TEXT_DESCRIPTION', '<b>Billplz ZenCart Module</b> is a service to help Malaysian merchant to sell online.<br><br> Visit <a href=http://www.billplz.com target=_blank>http://www.billplz.com</a> for more info regarding to Billplz Online Payment Gateway');
+    define('MODULE_PAYMENT_BILLPLZ_TEXT_ADMIN_TITLE', 'Billplz Payment Gateway');
+    define('MODULE_PAYMENT_BILLPLZ_TEXT_DESCRIPTION', '<b>Billplz for ZenCart Module</b>.<br><br> Visit <a href=http://billplz.com/join/8ant7x743awpuaqcxtqufg target=_blank>http://www.billplz.com</a> to Sign Up');
     define('MODULE_PAYMENT_BILLPLZ_TEXT_ERROR_MESSAGE', 'Your transaction has been cancelled or declined.  Please try again');
-    define('MODULE_PAYMENT_BILLPLZ_MARK_BUTTON_IMG', '<img src=images/logo_billplz.gif border=0 alt=Billplz Online Payment Gateway title=Billplz Online Payment Gateway>');
-    define('MODULE_PAYMENT_BILLPLZ_TEXT_TITLE','Billplz Online Payment Gateway(Visa, MasterCard, Maybank2u, MEPS, FPX, etc)');
+    define('MODULE_PAYMENT_BILLPLZ_MARK_BUTTON_IMG', '<img src=images/logo_billplz.gif border=0 alt=Billplz Payment Gateway title=Billplz Payment Gateway>');
+    define('MODULE_PAYMENT_BILLPLZ_TEXT_TITLE','Billplz (Maybank2u, CIMB Clicks, Bank Islam, RHB, Hong Leong Bank, Bank Muamalat, Public Bank, Alliance Bank, Affin Bank, AmBank, Bank Rakyat, UOB, Standard Chartered)');
 
 ob_end_clean();
 
