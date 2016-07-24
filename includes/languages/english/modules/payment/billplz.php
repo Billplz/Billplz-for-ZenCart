883794dd03fd5412fee231de800b5d066c93ac8d
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2016-07-25 07:38:46 +0800

    Update billplz.php

--- includes/languages/english/modules/payment/billplz.php
+++ includes/languages/english/modules/payment/billplz.php
@@ -11,7 +11,7 @@ ob_start();
 
     define('MODULE_PAYMENT_BILLPLZ_TEXT_CATALOG_TITLE', 'Internet Banking (FPX) Payment');
     define('MODULE_PAYMENT_BILLPLZ_TEXT_ADMIN_TITLE', 'Billplz Payment Gateway');
-    define('MODULE_PAYMENT_BILLPLZ_TEXT_DESCRIPTION', '<b>Billplz for ZenCart Module</b>.<br><br> Visit <a href=http://billplz.com/join/8ant7x743awpuaqcxtqufg target=_blank>http://www.billplz.com</a> to Sign Up');
+    define('MODULE_PAYMENT_BILLPLZ_TEXT_DESCRIPTION', '<b>Billplz for ZenCart Module</b>.<br><br> Visit <a href=http://bit.ly/1S9irKo target=_blank>http://www.billplz.com</a> to Sign Up');
     define('MODULE_PAYMENT_BILLPLZ_TEXT_ERROR_MESSAGE', 'Your transaction has been cancelled or declined.  Please try again');
     define('MODULE_PAYMENT_BILLPLZ_MARK_BUTTON_IMG', '<img src=images/logo_billplz.gif border=0 alt=Billplz Payment Gateway title=Billplz Payment Gateway>');
     define('MODULE_PAYMENT_BILLPLZ_TEXT_TITLE','Billplz (Maybank2u, CIMB Clicks, Bank Islam, RHB, Hong Leong Bank, Bank Muamalat, Public Bank, Alliance Bank, Affin Bank, AmBank, Bank Rakyat, UOB, Standard Chartered)');
